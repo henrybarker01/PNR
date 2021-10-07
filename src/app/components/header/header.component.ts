aa16eec8d5ab688c2ffbb3e3d8537bf8980a9a7a
Henry Barker <henry.barker@britehouse.co.za>
2021-10-07 16:14:30 +0200

    header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  selectedMenuItem: string = 'dashboard';
+
+  constructor(private readonly router: Router) {}
+
+  selectMenuItem(selectedItem: string) {
+    this.selectedMenuItem = selectedItem;
+    this.router.navigateByUrl(selectedItem);
+  }
+}
