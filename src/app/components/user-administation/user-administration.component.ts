ba35e9a21de38ac96c9f273855fdcfd8be853a18
Henry Barker <henry.barker@britehouse.co.za>
2021-10-08 08:51:02 +0200

    cleanup

--- src/app/components/user-administation/user-administration.component.ts
+++ src/app/components/user-administation/user-administration.component.ts
@@ -1,10 +1,10 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector:'app-user-administration',
-    templateUrl:'./user-administration.component.html',
-    styleUrls:['./user-administration.component.scss']
+  selector: 'app-user-administration',
+  templateUrl: './user-administration.component.html',
+  styleUrls: ['./user-administration.component.scss'],
 })
-export class UserAdministrationComponent{
+export class UserAdministrationComponent {
     
 }
