95283db5bbbb7dbcce11772b11c59adc9dd9f382
Henry Barker <henry.barker@britehouse.co.za>
2021-10-09 07:08:30 +0200

    update

--- src/app/components/user-administation/user-administration.component.ts
+++ src/app/components/user-administation/user-administration.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { async } from '@angular/core/testing';
+import { Client } from '@microsoft/microsoft-graph-client';
+import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
 import { UserAdministrationService } from 'src/app/services/user-administration/user-administartion.service';
 
 @Component({
@@ -9,10 +12,33 @@ import { UserAdministrationService } from 'src/app/services/user-administration/
 export class UserAdministrationComponent implements OnInit {
   //users: microsoftgraph.User[] = [];
 
-  constructor(private readonly userAdministrationService: UserAdministrationService) {}
+  constructor(
+    private readonly userAdministrationService: UserAdministrationService,
+    private readonly authenticationService: AuthenticationService
+  ) {}
 
-  async ngOnInit() {
-   // const user = await this.userAdministrationService.getUser();
-    //if (user) this.users.push(user);
+  ngOnInit() {
+    this.GetUser();
+  }
+  public async GetUser(): Promise<string> {
+    const graphClient = Client.init({
+      authProvider: async (done) => {
+        let token = await this.authenticationService.getToken().catch((reason) => {
+          done(reason, null);
+        });
+
+        if (token) {
+          token.subscribe((result) => {
+            done(null, result.accessToken);
+          });
+        } else {
+          done('', null);
+        }
+      },
+    });
+
+    const user = await graphClient.api('/me').get();
+    console.log(user);
+    return '';
   }
 }
