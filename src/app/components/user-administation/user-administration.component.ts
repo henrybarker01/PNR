4fc8861e6f608dd0ed25791a90632c6301600279
Henry Barker <henry.barker@britehouse.co.za>
2021-10-11 13:36:44 +0200

    cleanup

--- src/app/components/user-administation/user-administration.component.ts
+++ src/app/components/user-administation/user-administration.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { async } from '@angular/core/testing';
 import { MatDialog } from '@angular/material/dialog';
 import { Client } from '@microsoft/microsoft-graph-client';
 import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
 import { UserAdministrationService } from 'src/app/services/user-administration/user-administartion.service';
 import { DialogComponent } from '../dialog/dialog.component';
+import { MatTable } from '@angular/material/table';
 
 export interface PeriodicElement {
   name: string;
@@ -13,11 +14,7 @@ export interface PeriodicElement {
   symbol: string;
 }
 
-const ELEMENT_DATA = [
-  { name: 'Henry', surname: 'Barker', username: 'henry.barker@britehouse.co.za', role: 'User Administation' },
-  { name: 'Brandon', surname: 'Slabbert', username: 'brandon.slabbert@britehouse.co.za', role: 'User Administation' },
-  { name: 'Juan', surname: 'Lurie', username: 'juan.lurie@britehouse.co.za', role: 'User' },
-];
+const ELEMENT_DATA = [,];
 
 @Component({
   selector: 'app-user-administration',
@@ -25,8 +22,9 @@ const ELEMENT_DATA = [
   styleUrls: ['./user-administration.component.scss'],
 })
 export class UserAdministrationComponent implements OnInit {
+  @ViewChild(MatTable) table: MatTable<PeriodicElement> | undefined;
   displayedColumns: string[] = ['name', 'surname', 'username', 'role', 'action'];
-  dataSource = ELEMENT_DATA;
+  dataSource: any[] = [];
   //users: microsoftgraph.User[] = [];
   // users: any[] = [
   // ];
@@ -34,16 +32,27 @@ export class UserAdministrationComponent implements OnInit {
 
   addUsers() {
     const dialogRef = this.dialog.open(DialogComponent);
-
     dialogRef.afterClosed().subscribe((result) => {
       console.log(`Dialog result: ${result}`);
+      this.dataSource.push({ name: 'Henry', surname: 'Demo', username: 'henry.demo@bidtravel.co.za', role: 'User' });
+      if (this.table) this.table.renderRows();
     });
   }
 
-  removeUser(userName: string) {}
+  removeUser(userName: string) {
+    this.dataSource = this.dataSource.filter((x) => x.username !== userName);
+    if (this.table) this.table.renderRows();
+  }
 
   ngOnInit() {
-    //this.GetUser();
+    this.dataSource.push({ name: 'Henry', surname: 'Barker', username: 'henry.barker@britehouse.co.za', role: 'User Administation' });
+    this.dataSource.push({
+      name: 'Brandon',
+      surname: 'Slabbert',
+      username: 'brandon.slabbert@britehouse.co.za',
+      role: 'User Administation',
+    });
+    this.dataSource.push({ name: 'Juan', surname: 'Lurie', username: 'juan.lurie@britehouse.co.za', role: 'User' });
   }
   public async GetUser() {
     // Promise<string> {
